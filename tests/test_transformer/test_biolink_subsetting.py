43abfb88d9a2d0157e1ef9429b4e24c31416abac
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2024-11-13 14:58:24 -0800

    add fixtures to test

--- tests/test_transformer/test_biolink_subsetting.py
+++ tests/test_transformer/test_biolink_subsetting.py
@@ -1,32 +1,56 @@
 from pathlib import Path
-from typing import List
 from pytest import fixture
 
 from linkml_runtime.dumpers import yaml_dumper
-import pytest
-from pprint import pprint
 from linkml_map.datamodel.transformer_model import TransformationSpecification, ClassDerivation, SlotDerivation
 from linkml_map.inference.schema_mapper import SchemaMapper
 from linkml_map.session import Session
 from linkml_runtime.utils.schemaview import SchemaView
 from src.linkml_map.utils.loaders import load_specification
-from linkml_map.transformer.transformer import Transformer
-from linkml_map.utils.multi_file_transformer import Transformation
-from linkml_model.meta import ClassDefinition
-from linkml.utils.schema_builder import SchemaBuilder
 from linkml_runtime.utils.formatutils import camelcase
 
 REPO_ROOT = Path(__file__).resolve().parent.parent
 
 
+def get_biolink_class_derivations(sv, subset_classes) -> dict:
+    """
+    Function to get Biolink class definitions
+
+    :param sv: SchemaView object
+    :param subset_classes: List of classes to subset
+    :return: Dictionary of class derivations incl slot derivations
+    """
+    # Example implementation to fetch class definitions
+    # This should be replaced with the actual implementation
+    class_derivations ={}
+    for class_name in subset_classes:
+        class_derivation = ClassDerivation(populated_from=class_name,
+                                           name=camelcase(class_name))
+        for slot in sv.get_class(class_name).slots:
+            slot_derivation = SlotDerivation(populated_from=slot, name=camelcase(slot))
+            class_derivation.slot_derivations[slot] = slot_derivation
+        class_derivations[camelcase(class_name)] = class_derivation
+    return class_derivations
+
+
 @fixture
 def biolink_schema():
+    """
+    Fixture to load Biolink schema
+
+    :return: SchemaView object named `biolink-schema`
+    """
+
     schema_url = "https://raw.githubusercontent.com/biolink/biolink-model/master/biolink-model.yaml"
     sv = SchemaView(schema_url)
     return sv
 
 def test_biolink_subsetting_manual(biolink_schema):
+    """
+    Test to subset the Biolink schema manually
 
+    :param biolink_schema: Fixture to load Biolink schema
+    """
     transform_file = REPO_ROOT / "input/examples/biolink/transform/biolink-example-profile.transform.yaml"
     # Initialize Session and SchemaBuilder
     session = Session()
@@ -52,6 +76,12 @@ def test_biolink_subsetting_manual(biolink_schema):
 
 
 def test_biolink_subset_auto(biolink_schema):
+    """
+    Test to subset the Biolink schema automatically by deriving the class definitions from biolink
+    via a collection of class names to subset.
+
+    :param biolink_schema: Fixture to load Biolink schema
+    """
     # Initialize Session and SchemaBuilder
     session = Session()
 
@@ -85,19 +115,4 @@ def test_biolink_subset_auto(biolink_schema):
     for class_name in transformed_sv.all_classes():
         print(class_name)
 
-
-
-# Function to get Biolink class definitions
-def get_biolink_class_derivations(sv, subset_classes) -> dict:
-    # Example implementation to fetch class definitions
-    # This should be replaced with the actual implementation
-    class_derivations ={}
-    for class_name in subset_classes:
-        class_derivation = ClassDerivation(populated_from=class_name,
-                                           name=camelcase(class_name))
-        for slot in sv.get_class(class_name).slots:
-            slot_derivation = SlotDerivation(populated_from=slot, name=camelcase(slot))
-            class_derivation.slot_derivations[slot] = slot_derivation
-        class_derivations[camelcase(class_name)] = class_derivation
-    return class_derivations
 
